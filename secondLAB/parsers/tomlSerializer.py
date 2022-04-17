d21500d7d312bc6ba6607e07ae4c58066627b1b1
guzeradimazz <Guzerashop@mail.ru>
2022-04-17 22:06:40 +0300

    unittesting

--- secondLAB/parsers/tomlSerializer.py
+++ secondLAB/parsers/tomlSerializer.py
@@ -3,8 +3,15 @@ import toml
 
 class TomlSerializer(Serializer):
 
-    def dumps(entity,path):
-        return toml.dumps(entity,path)
+    def dumps(self, obj):
+        obj_dict = super().dumps(obj)
+        if 'code' in obj_dict:
+            for (key, value) in obj_dict['code'].items():
+                if key == 'co_consts':
+                    value = list(value)
+                    obj_dict['code'][key] = value
 
-    def loads(entity):
-        return toml.loads(entity)
+        return toml.dumps(obj_dict)
+
+    def loads(self, toml_string):
+        return super().loads(toml.loads(toml_string))
--- secondLAB/unitTests/testMain.py
+++ secondLAB/unitTests/testMain.py
@@ -1,4 +1,3 @@
-from typing import Type
 import unittest
 from main import main
 
